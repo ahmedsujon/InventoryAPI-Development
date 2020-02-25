8492ea5e1b7f747497efbe56c1604e3b66f86f79
Sujon Ahmed <ahmedsujon050@gmail.com>
2020-02-25 17:26:23 +0600

    Class Model Create

---
+++ database/factories/ClasseFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Classe;
+use Faker\Generator as Faker;
+
+$factory->define(Classe::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
